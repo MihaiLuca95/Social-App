ebe52860674b4f3577af1f3e5ef64595036fd34d
MihaiLuca95 <lucamihai1995@gmail.com>
2022-03-19 07:32:39 +0200

    add service

--- app-front/src/components/model/User.js
+++ app-front/src/components/model/User.js
@@ -5,13 +5,6 @@ class User {
     this.avatar = avatar;
     this.lastOnline = lastOnline;
   }
-
-  // dynamic / non-static 
-  static get() {
-    return fetch('http://localhost:3001/api/user')
-      .then(response => response.json())
-      .then(data => new User(data.nickname, data.avatar, data.lastOnline));
-  }
 }
 
 export default User  ;
